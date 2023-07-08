8201d8d16a9fe60f81e12036d9181354486d6e01
Tonmoy777 <tomsarker210@gmail.com>
2023-07-08 19:13:08 +0600

    Assignment 9

---
+++ app/data.js
@@ -0,0 +1,143 @@
+const posts = [
+  {
+    id: 1,
+    image:
+      "https://scontent.fdac5-2.fna.fbcdn.net/v/t39.30808-6/357698394_253487020771714_4331803611420433276_n.jpg?_nc_cat=104&cb=99be929b-3346023f&ccb=1-7&_nc_sid=0debeb&_nc_eui2=AeHJ4yqlMWkiYolVM_GIeeGUaglCM5PvgTpqCUIzk--BOoTNRq8u9iHE-ceGKhICX88KeplH-CZyLOyK2qaMS9vv&_nc_ohc=ACBEFILOEZ4AX-T_iy7&_nc_ht=scontent.fdac5-2.fna&oh=00_AfAMlJJCtJtG0doybgBgF-NkAmIH2BIfvrdE_oErlObbRA&oe=64AB4909",
+    content: "একদিন পুরো ছবিটি স্মৃতি হয়ে যাবে।",
+    like: 200,
+    Comment: 12,
+    timeaAgo: "6 hours",
+    author: {
+      name: "Triangle Lineage ",
+      photo:
+        "https://scontent.fdac5-1.fna.fbcdn.net/v/t39.30808-6/336353738_101484472898354_1544692658362776373_n.jpg?_nc_cat=111&cb=99be929b-3346023f&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeG90zTjLAl4I0UhEt-Din4PcXrR-wkVXbpxetH7CRVduoKJwjVZU2muNefy5Ueex0eFFH1IlCvyWQTmCWS8NqGb&_nc_ohc=uVBpvdpLeMAAX8cU-gD&_nc_ht=scontent.fdac5-1.fna&oh=00_AfAXsywRYXrp2cWOFi1zLYj_md7t1cNdfTspCnGhnh3eGg&oe=64ABBC33",
+    },
+  },
+  {
+    id: 2,
+    image:
+      "https://scontent.fdac5-1.fna.fbcdn.net/v/t39.30808-6/357490785_720223059909943_8350813111327289262_n.jpg?_nc_cat=109&cb=99be929b-3346023f&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeF3Z_wQBjEGtpcQMfgbUlwhaX6Fl8EQ9DppfoWXwRD0OpGwS7Zko55oDOSv5zB-5x8Cccaov7qVWDvx-cRgGAEV&_nc_ohc=0_86rRdMAs0AX-aZV9k&_nc_oc=AQklP-UPCLyktAtOEU8Pg9dnFCoUPZeajy5q_IvTZbAOO8aHm6v2fUMHvbfaZ37oBXk&_nc_ht=scontent.fdac5-1.fna&oh=00_AfBY_H8eEQnwHssr8mWEVcFZGBc8bRzaZ1ntGzvP8RcahQ&oe=64ABB485",
+    content: "বেলাশেষে, কেউ থাকেনা পাশে... ",
+    like: "4.2K",
+    Comment: 297,
+    timeaAgo: "4 hours",
+    author: {
+      name: "বেলা বোস",
+      photo:
+        "https://scontent.fdac5-1.fna.fbcdn.net/v/t39.30808-6/286184516_462662782332640_8026727374445654956_n.jpg?_nc_cat=1&cb=99be929b-3346023f&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGmL_G-6_rQ-7byQze2NDopy0DyKx8-UZrLQPIrHz5Rms97oO_LxsLiOArU6yXmFB8sZZdccZMrV9j621tEGqnc&_nc_ohc=uyfMBMLViWoAX9xKj79&_nc_ht=scontent.fdac5-1.fna&oh=00_AfARyDXfGh8wCJOtL4FOz08nlNxqd26C3PHaa84eoLRMlg&oe=64AB6E92",
+    },
+  },
+  {
+    id: 3,
+    image:
+      "https://scontent.fdac5-1.fna.fbcdn.net/v/t39.30808-6/357742729_834843794675853_3726219863852718464_n.jpg?_nc_cat=1&cb=99be929b-3346023f&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeE8-xHcxj5bPjv7OQwbEdOHoRqxbpDo9XWhGrFukOj1daNCvvz7DPHrLHvL44Xjh09Ybhl3XLez6tER-3I87RFW&_nc_ohc=8fzkQXXlkiIAX8scyVm&_nc_ht=scontent.fdac5-1.fna&oh=00_AfDtdeqA-e2H1-rsOXN6mm9YjXUMSHoXkenoY5oC6Aew-g&oe=64ACB0A6",
+    content: `প্রিয় তামিম ভাই,
+    আপনি দেশকে দীর্ঘ ১৬ বছর ধরে সার্ভ করেছেন। কত দারুণ সব মুহূর্ত উপহার দিয়েছেন আমাদের! জহির খানকে ডাউন দ্য উইকেটে মারা ছক্কায় নিজের আবির্ভাবের জানান দেয়া, লর্ডসের অনার্স বোর্ডে বলেকয়ে নিজের নাম ওঠানো, এশিয়া কাপে টানা চার ম্যাচে ফিফটি, চ্যাম্পিয়ন্স ট্রফিতে রানের বন্যা বইয়ে দেয়া, ভাঙা হাত নিয়ে সেই অসামান্য বীরত্ব- কোনটা রেখে কোনটার কথা বলব? খেলাটার প্রতি আপনার প্যাশন, আপনার আবেগ সব সময়ই আমাদের হৃদয় ছুঁয়ে গেছে
+    `,
+    like: "32K",
+    Comment: 779,
+    timeaAgo: "3 hours",
+    author: {
+      name: "Siam Ahmed",
+      photo:
+        "https://scontent.fdac5-1.fna.fbcdn.net/v/t39.30808-6/355858933_827121665448066_2535697471794449018_n.jpg?_nc_cat=1&cb=99be929b-3346023f&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHN-77DljhLxPyAhiACAZMNBvevQxwCKC8G969DHAIoL-fb7PrDXM546tJHpOyEw-1JiNQZaOARbF_tZ5X1XxZX&_nc_ohc=XEUdlg4UKtoAX_Of-2C&_nc_ht=scontent.fdac5-1.fna&oh=00_AfAmQBbdO5IfGwPk_5XIKPHeQlRZhZwm97uuUYrrBIaPIg&oe=64AC8B6A",
+    },
+  },
+  {
+    id: 4,
+    image:
+      "https://scontent.fdac5-1.fna.fbcdn.net/v/t39.30808-6/357717740_660363366125158_5937188714188361383_n.jpg?stp=dst-jpg_p960x960&_nc_cat=1&cb=99be929b-3346023f&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGVOxr5D3WDunTNAuWRFpvu0J80GL0jSnzQnzQYvSNKfF1-Gte-l_keQu5BSs6EvNc0Kgfj04lV9f5_hXtc7_lH&_nc_ohc=i45hm5k9t34AX9l3SHk&_nc_ht=scontent.fdac5-1.fna&oh=00_AfAq3w9eva9NNOIg1Kpz45hAlJH6iJaqYWGE6_R3ecYOSw&oe=64ABA332",
+    content: "Reunion in Paris.",
+    like: "2K",
+    Comment: 17,
+    timeaAgo: "3 hours",
+    author: {
+      name: "Plaantik",
+      photo:
+        "https://scontent.fdac5-1.fna.fbcdn.net/v/t39.30808-6/315970661_5959241600774721_2868385138446175389_n.jpg?_nc_cat=1&cb=99be929b-3346023f&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHT4R3WTQ1kbZQ7wUxOY12MeT0JNbXLKV15PQk1tcspXaC5LG6SjK4pU1_WXAy4c_pdaWZRflwQ3R4fCrx5M62Q&_nc_ohc=RqOqeAfZRBMAX_PcWcP&_nc_ht=scontent.fdac5-1.fna&oh=00_AfCW4-h5RyrLTpGiBQi2i4OG10-EJ0qs57MB03fpOZm3ew&oe=64ABE607",
+    },
+  },
+  {
+    id: 5,
+    image:
+      "https://scontent.fdac5-1.fna.fbcdn.net/v/t39.30808-6/347428402_168543882881643_8344690783435127577_n.jpg?_nc_cat=111&cb=99be929b-3346023f&ccb=1-7&_nc_sid=5cd70e&_nc_eui2=AeGuiaqxny0V3plzl302bq3r_45AST5YneD_jkBJPlid4ECWUnDHY4lCZXPvZZvz1ockCSQLPhUMNEFgEY0q_F61&_nc_ohc=k7SF5QaeJk8AX-S21RX&_nc_ht=scontent.fdac5-1.fna&oh=00_AfAEyzwO6bkd9wrE10MPNahEgUuVGXu4tIk3If94sWOcJw&oe=64AB27CE",
+    content: "Follow Thom Code for more 👌.",
+    like: "137",
+    Comment: 2,
+    timeaAgo: "2 hours",
+    author: {
+      name: "Thomas Chinyere",
+      photo:
+        "https://scontent.fdac5-2.fna.fbcdn.net/v/t39.30808-6/336378809_542143858036998_7549383044359478463_n.jpg?_nc_cat=104&cb=99be929b-3346023f&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHbYRrtWn2DUc6V_O03Uuq8_Gon0ylmSBP8aifTKWZIE8iGpoHCi9yvcKE1b8T5ZmBqw6N0kUHe3kFN-sZaJsCA&_nc_ohc=zDlrOwi0d_4AX9AiFpM&_nc_ht=scontent.fdac5-2.fna&oh=00_AfDa0lE9vNGlNAmsqplBm3aG2ZHhBIH-rzqDpbXKPF1MGA&oe=64ABB816",
+    },
+  },
+  {
+    id: 6,
+    image:
+      "https://scontent.fdac5-2.fna.fbcdn.net/v/t39.30808-6/357448712_3433921776876852_1163350718052245425_n.jpg?stp=dst-jpg_p843x403&_nc_cat=102&cb=99be929b-3346023f&ccb=1-7&_nc_sid=5cd70e&_nc_eui2=AeGA1SiWnbslCPiQQI9BXnykYoKRhGKo7opigpGEYqjuiq98dc0bJ4cc5zlHSUgNJGNVckkQRKZ87oWrHSyObODd&_nc_ohc=i-Ira8fPAPAAX8AzNkJ&_nc_ht=scontent.fdac5-2.fna&oh=00_AfBJZ7c4QK2Ou3FEjc9KSXi47NOIhiuM4901wXaoX1lGEA&oe=64AC4BE8",
+    content: "🤧",
+    like: "178",
+    Comment: 41,
+    timeaAgo: "1 hours",
+    author: {
+      name: "Edward Tang",
+      photo:
+        "https://scontent.fdac5-1.fna.fbcdn.net/v/t39.30808-1/355469922_1698936330607501_448750442226880843_n.jpg?stp=cp6_dst-jpg_p320x320&_nc_cat=106&cb=99be929b-3346023f&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeGe2sO_ZHfxaopKaR45F-TVlbLcpP2cBPSVstyk_ZwE9H2HoQr4EoiOGVZEGa8Uy6rJKtNDz1YLYru4izfOeZYj&_nc_ohc=OUUTBD7mPQ8AX_Wp1cl&_nc_ht=scontent.fdac5-1.fna&oh=00_AfBFPdzFK1UFKvIuE3hey67VQ_A5mbtqzYy3PgcHg-1cMA&oe=64AB471B",
+    },
+  },
+  {
+    id: 7,
+    image:
+      "https://scontent.fdac5-1.fna.fbcdn.net/v/t39.30808-6/357718327_3433119433623753_4995244177964037225_n.jpg?_nc_cat=109&cb=99be929b-3346023f&ccb=1-7&_nc_sid=5cd70e&_nc_eui2=AeHvcB-XD4u4g8dh7r3A0Ht-QFXSrshB6bBAVdKuyEHpsPPDXK6yjSIqW5qXTr_juSYamMfYE4vufSjTMRNp6b_l&_nc_ohc=oFsWBvPLJHAAX98wCSA&_nc_ht=scontent.fdac5-1.fna&oh=00_AfA4DgHDgwjKI8n2O3L7qbK6VVTQCrWQTNdrHWsf5rjGMA&oe=64AB2C70",
+    content: "😐",
+    like: "2.4K",
+    Comment: 56,
+    timeaAgo: "44 minutes",
+    author: {
+      name: "Abdelrhiim Abdelmoneim",
+      photo:
+        "https://scontent.fdac5-2.fna.fbcdn.net/v/t39.30808-6/343416735_1351154032127618_4868350926687174480_n.jpg?_nc_cat=108&cb=99be929b-3346023f&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeG0OABrERyHQ3t__uqIqFu7gsDoeuVjUsWCwOh65WNSxUP2iK-MibtZ4iUoGAdr5pGMYQYWqqxlutRsT6lck2Bh&_nc_ohc=oIcNuXQcvPgAX9G-fUl&_nc_ht=scontent.fdac5-2.fna&oh=00_AfBoCuhXW-4ZVMa61rJSddEhSwF22FTeT_Qom25GAoe6rA&oe=64ACA0F7",
+    },
+  },
+  {
+    id: 8,
+    image:
+      "https://scontent.fdac5-2.fna.fbcdn.net/v/t39.30808-6/357460120_826996185451861_2694079370521170240_n.jpg?_nc_cat=107&cb=99be929b-3346023f&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeGODovv3HuRlS-uWiZPgfDmeMiFg5MQrM14yIWDkxCszUEtU_cOK4zYHYsTLmQn2Q43QFKSPbttmSwtq87sXbbP&_nc_ohc=W4q3_G_C8jcAX-wUnDT&_nc_ht=scontent.fdac5-2.fna&oh=00_AfAQC2MKwdA27lqMeK4ocvAzbppb27r0cVj2Me2GkqVvKQ&oe=64ACBB7D",
+    like: "137",
+    Comment: 17,
+    timeaAgo: "25 minutes",
+    author: {
+      name: "Faizlamir Ekta Limit Ase",
+      photo:
+        "https://scontent.fdac5-2.fna.fbcdn.net/v/t39.30808-1/293576569_599688284849320_8702753690473405064_n.jpg?stp=dst-jpg_s320x320&_nc_cat=104&cb=99be929b-3346023f&ccb=1-7&_nc_sid=c6021c&_nc_eui2=AeHNR06O6i7WJXXbzjxPW4zJv1G306hl6zq_UbfTqGXrOrlNpKzu2SWp0uz8i0y7U_l1LhK9U3N0UWfNnLC82ACB&_nc_ohc=pP-1v27GbysAX_ZfV-E&_nc_ht=scontent.fdac5-2.fna&oh=00_AfCgo8k8UMr_oN7gUKXeBqEK24D0u9gWaYUU41fz5Ex-ow&oe=64AC8979",
+    },
+  },
+  {
+    id: 9,
+    image:
+      "https://scontent.fdac5-1.fna.fbcdn.net/v/t39.30808-6/358033634_1303725637218130_9021536939794472977_n.jpg?_nc_cat=100&cb=99be929b-3346023f&ccb=1-7&_nc_sid=5cd70e&_nc_eui2=AeFYsqoCdvvmwX1M9MOkafSPfiIYTAAP_z9-IhhMAA__P9Sa_wNm3KqEZXH8a-eJHeBzHAEz0urxiqGnT6fuRne5&_nc_ohc=h2UXO4CPgY0AX_K2Xkn&_nc_ht=scontent.fdac5-1.fna&oh=00_AfAH8Y8tRxIlkm9Zk0o4Ykm1GKowX1JgYWWhxpVVzCLfug&oe=64AC0512",
+    content: "🙃",
+    like: "130",
+    Comment: 27,
+    timeaAgo: "16 minutes",
+    author: {
+      name: "Fakhar Abbas Kamario",
+      photo:
+        "https://scontent.fdac5-2.fna.fbcdn.net/v/t39.30808-1/347583216_1302073924049968_2553810457133572665_n.jpg?stp=c0.0.320.320a_dst-jpg_p320x320&_nc_cat=104&cb=99be929b-3346023f&ccb=1-7&_nc_sid=f67be1&_nc_eui2=AeFiAbywrfXuV2buBO7QeylcCfpcVpoF_fUJ-lxWmgX99cyHTTEN3xmusnKc7A63UqVz475IVHV4rFBRTyk2QecK&_nc_ohc=cqBJM6W5us8AX-8rCMA&_nc_ht=scontent.fdac5-2.fna&oh=00_AfB8axzFI9DHLCWJKRePibGZem0oHITUK5rD8oGhq1Id3g&oe=64AB4B52",
+    },
+  },
+  {
+    id: 10,
+    image:
+      "https://scontent.fdac5-1.fna.fbcdn.net/v/t39.30808-6/358077794_247431598018730_3581334939053261643_n.jpg?_nc_cat=100&cb=99be929b-3346023f&ccb=1-7&_nc_sid=5cd70e&_nc_eui2=AeFQXuKAWw65Gpx1MlmLLLX6rQ7EwCk6pbetDsTAKTqlt0TD68gSkhfXFBvnxyy6RuEyV-_nBloxWD5h3DvoKiHK&_nc_ohc=GURIL11sfvcAX-uS8_d&_nc_ht=scontent.fdac5-1.fna&oh=00_AfAs2HcEE9rcCO7TuyEevBZ-kuzYzU4YGh7_1tmSb4P9hg&oe=64AAE872",
+    content: "Your future job😁",
+    like: "979",
+    Comment: 53,
+    timeaAgo: "10 minutes",
+    author: {
+      name: "Anmol Maryam",
+      photo:
+        "https://scontent.fdac5-1.fna.fbcdn.net/v/t39.30808-6/326032210_1201430184076896_1564204024286592241_n.jpg?_nc_cat=106&cb=99be929b-3346023f&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeH60Uk8NNlYszHTToJ0028Vef0c7Ll0MMJ5_RzsuXQwwqB3jFW_8Z_mldUjMpEHMmqKwjdspgakl59j4qrnnxDO&_nc_ohc=1OAr-Mcs1fEAX91xnae&_nc_ht=scontent.fdac5-1.fna&oh=00_AfDX3GKFfcgMvBHKx08RMDQFBjOxTrtumLRQ4jOaBS_Auw&oe=64AB4EB7",
+    },
+  },
+];
